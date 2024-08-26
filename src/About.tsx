9b4a9d309b74943cb38a34bae1de97df76c25081
Ben Capp <bentucker333@gmail.com>
2024-08-26 13:06:37 -0400

    rearrange files

--- src/About.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About: React.FC = () => {
-  return <div>Contact at bentucker333@gmail.com to purchase a print or if you have any questions.</div>;
-};
-
-export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-import Nav from "./Nav.tsx";
-import Home from "./Home.tsx";
-import About from "./About.tsx";
-import Gallery from "./Gallery.tsx";
+import Nav from "./components/Nav.tsx";
+import Home from "./components/Home.tsx";
+import About from "./components/About.tsx";
+import Gallery from "./components/Gallery.tsx";
+import Shop from "./components/Shop.tsx";
 
 const App: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
     </Router>
     // <div className="App">
